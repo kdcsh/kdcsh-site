b53515da9f77c9eb80b56114dbcbce713c5133ee
olehpodusov <podusov.oleg1@gmail.com>
2025-01-14 10:33:36 +0200

    MSEC-reform
    MSEC-reform

--- styles.js
+++ styles.js
@@ -53,3 +53,21 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     };
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var modal3 = document.getElementById("myModal3");
+    var btn3 = document.getElementById("myBtn3");
+    var span3 = modal3.getElementsByClassName("close")[0];
+
+    btn3.onclick = function() {
+        modal3.style.display = "block";
+    };
+    span3.onclick = function() {
+        modal3.style.display = "none";
+    };
+    window.onclick = function(event) {
+        if (event.target == modal3) {
+            modal3.style.display = "none";
+        }
+    };
+});
